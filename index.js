f22ce35f592cee9a2146f394781d62729373480b
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-10-30 21:58:26 +0800

    Fix bugs and improve coding

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var fs = require('hexo-fs');
 var path = require('path');
 var url = require("url");
 
-var generators = new Array();
 
 function registerFile(pathname, file) {
-  generators.push({
+  generators.push( {
     path: pathname,
     data: function () {
       return fs.createReadStream(file)
@@ -20,27 +19,31 @@ function registerDir(pathname, dir) {
   }, this);
 }
 
-hexo.extend.helper.register('live2d', function (config) {
-  var config = Object.assign(
-    {
-      model: "z16",
-      width: 150,
-      height: 300,
-      mobileShow: "true",
-      mobileWidth: 75,
-      mobileHeight: 150,
-      position: "right",
-      horizontalOffset: 0,
-      bottomOffset: -20,
-      className: "live2d",
-      id: "live2dcanvas",
-    },
-    config, 
-    hexo.config.live2d
-  );
 
+var generators = new Array();
+var config = Object.assign( {
+    model: "z16",
+    width: 150,
+    height: 300,
+    mobileShow: "true",
+    mobileWidth: 75,
+    mobileHeight: 150,
+    position: "right",
+    horizontalOffset: 0,
+    bottomOffset: -20,
+    className: "live2d",
+    id: "live2dcanvas",
+  },
+  config,
+  hexo.theme.config.live2d,
+  hexo.config.live2d
+);
+		  
+hexo.extend.helper.register('live2d', function() {
   return `
-    <canvas id="${config.id}" width="${config.width}" height="${config.height}" class="${config.className}"></canvas>
+	<div id="hexo-helper-live2d">
+      <canvas id="${config.id}" width="${config.width}" height="${config.height}" class="${config.className}"></canvas>
+	</div>
     <style>
       #${config.id} {
         position: fixed;
@@ -53,30 +56,25 @@ hexo.extend.helper.register('live2d', function (config) {
     <script src="live2d/device.min.js"></script>
     <script type="text/javascript">
     (function(){
-      if(device.mobile()){
-        if(${config.mobileShow}){
-          document.getElementById("${config.id}").width = ${config.mobileWidth};
-          document.getElementById("${config.id}").height = ${config.mobileHeight};
-          document.write('<script type="text/javascript" src="/live2d/script.js"><\\/script>');
-          document.write('<script>loadlive2d(${JSON.stringify(config.id)}, ${JSON.stringify(url.resolve("/live2d/assets/", config.model + ".model.json"))}, 0.5)<\\/script>');
-        }
-      }else{
-        document.write('<script type="text/javascript" src="/live2d/script.js"><\\/script>');
-        document.write('<script>loadlive2d(${JSON.stringify(config.id)}, ${JSON.stringify(url.resolve("/live2d/assets/", config.model + ".model.json"))}, 0.5)<\\/script>');
-      }
+      ${config.mobileShow ? `
+    if(device.mobile()){
+      document.getElementById("${config.id}").width = ${config.mobileWidth};
+      document.getElementById("${config.id}").height = ${config.mobileHeight};
+      document.write('<script type="text/javascript" src="/live2d/script.js"><\\/script>');
+      document.write('<script>loadlive2d(${JSON.stringify(config.id)}, ${JSON.stringify(url.resolve("/live2d/assets/", config.model + ".model.json"))}, 0.5)<\\/script>');
+    }else{
+      document.write('<script type="text/javascript" src="/live2d/script.js"><\\/script>');
+      document.write('<script>loadlive2d(${JSON.stringify(config.id)}, ${JSON.stringify(url.resolve("/live2d/assets/", config.model + ".model.json"))}, 0.5)<\\/script>');
+    }` : `
+    if(!device.mobile()){
+      document.write('<script type="text/javascript" src="/live2d/script.js"><\\/script>');
+      document.write('<script>loadlive2d(${JSON.stringify(config.id)}, ${JSON.stringify(url.resolve("/live2d/assets/", config.model + ".model.json"))}, 0.5)<\\/script>');
+    }`
+    }
     })();
     </script>
-  `
+`
 });
-
-
-var config = Object.assign(
-  {
-    model: "z16",
-  },
-  config, 
-  hexo.config.live2d
-);
 
 registerDir('live2d/assets/', path.resolve(__dirname, path.join('./assets/', config.model)));
 registerFile('live2d/script.js', path.resolve(__dirname, './dist/bundle.js'));
@@ -85,3 +83,4 @@ registerFile('live2d/device.min.js', path.resolve(__dirname, './dist/device.min.
 hexo.extend.generator.register('live2d', function (locals) {
   return generators;
 });
+
